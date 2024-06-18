cfd36e4fe67eb27442ca00e7a89f4176e84d6637
Alister Brumley <ali.b912@gmail.com>
2024-06-19 00:44:38 +1000

    updated search to work with movies, companies and people

--- tvdbSearch.py
+++ tvdbSearch.py
@@ -3,12 +3,43 @@ import typer
 from pprint import pprint
 
 
+def type_switch(db, type, id):
+    match type:
+        case "series":
+            return db.get_series(id)
+        case "company":
+            return db.get_company(id)
+        case "list":
+            return  # RE RUN SEARCH THROUGH LIST
+        case "movie":
+            return db.get_movie(id)
+        case "person":
+            return db.get_person(id)
+    # THESE SHOULD NOT OCCUR, UNCOMMENT TO RE-ENABLE
+        # case "season":
+        #     return db.get_season(id)
+        # case "episode":
+        #     return db.get_episode(id)
+        # case "artwork":
+        #     return db.get_artwork(id)
+        # case "character":
+        #     return db.get_character(id)
+
+
 def main(key: str):
     tvdb = tvdb_v4_official.TVDB(key)
 
     query = input("Search: ")
 
+    if not query:
+        print("no search term entered! exiting...")
+        exit(1)
+
     results = tvdb.search(query)
+
+    if not results:
+        print("no results! exiting...")
+        exit(1)
 
     for cnt, dict in enumerate(results):
         cnt = str(cnt)
@@ -19,14 +50,22 @@ def main(key: str):
         else:
             year = dict["year"]
 
+        year = dict["type"]
+
         print(cnt + ") " + name + " " + year)
 
+    # TODO MAKE FNC AND RECURSIVE RUN UNTIL SELECTION IN RANGE
     selection = int(input("Selection: "))
-    # ONLY WORKS WITH SERIES CURRENTLY
-    # HAVE TO CHECK ID FOR MOVIE/SERIES etc AND RETURN RESULT
-    # OR TYPE?
-    sel_result = tvdb.get_series(results[selection]["tvdb_id"])
-    pprint(sel_result)
+
+    sel_type = results[selection]["type"]
+    sel_id = results[selection]["tvdb_id"]
+
+    # TESTING ONLY, TO REMOVE WHEN COMPLETE
+    pprint(sel_type)
+
+    info = type_switch(tvdb, sel_type, sel_id)
+
+    pprint(info)
 
 
 if __name__ == "__main__":
