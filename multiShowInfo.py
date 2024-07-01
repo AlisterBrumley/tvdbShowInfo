7bd9f79b3efa19bbdddd4e4fc085c02d8af3ce64
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-07-01 20:22:23 +1000

    function for writing, added some extra get's and also comments

--- multiShowInfo.py
+++ multiShowInfo.py
@@ -3,34 +3,58 @@ import typer
 import json
 from pathlib import Path
 
+#                       NOTES!!!!!!
+# # OUTPUTTING TO JSON
+# # TWO DIFFERENT WAYS OF DOING, DUMP AND DUMPS
+# # INDENT HAS TO BE >0 OTHERWISE IT PRINTS WEIRD
+# # ENSURE_ASCII HAS TO BE FALSE
+# # TO ENSURE NON-ENGLISH GETS PRINTED
+# DUMPS IS ACTUALLY FOR STRINGS
+# BUT WE CAN SEND IT TO A FILE
+# series = tvdb.get_series(tvdb_id)
+# outSeries.write_text(json.dumps(series, indent=4, ensure_ascii=False))
+
+# DUMPS IS SUPPOSED TO BE FOR FILES
+# BUT FILE HAS TO BE OPENED WITH 'W' FIRST
+# series_extended = tvdb.get_series_extended(tvdb_id)
+# json.dump(series_extended, outSE.open("w"), indent=4, ensure_ascii=False)
+
+
+def output(outfile, info):
+    outfile.write_text(json.dumps(info, indent=4, ensure_ascii=False))
+
 
 def main(key: str, tvdb_id: int):
 
+    # #### AUTH
     tvdb = tvdb_v4_official.TVDB(key)
 
+    # #### SET BASE PATH
     outPath = Path.cwd() / "dump"
+
+    # #### SET OUT PATH
+    # maybe define these all as part of a list and then iterate through writing them
+    # , after made a tuple? why?
     outSeries = outPath / "series.json"
-    outSE = outPath / "series_extended.json"
+    outSEx = outPath / "series_extended.json"
+    outSEp = outPath / "series_episodes.json"
+    outSEpD = outPath / "series_episodes_default.json"
+    outSEpA = outPath / "series_episodes_alternate.json"
+    outSTr = outPath / "series_translation.json"
 
-    # outSeries.touch()
-    # outSE.touch()
-
-    # # OUTPUTTING TO JSON
-    # # TWO DIFFERENT WAYS OF DOING, DUMP AND DUMPS
-    # # INDENT HAS TO BE >0 OTHERWISE IT PRINTS WEIRD
-    # # ENSURE_ASCII HAS TO BE FALSE
-    # # TO ENSURE NON-ENGLISH GETS PRINTED
-    # DUMPS IS ACTUALLY FOR STRINGS
-    # BUT WE CAN SEND IT TO A FILE
+    # #### GETTING INFO
     series = tvdb.get_series(tvdb_id)
-    outSeries.write_text(json.dumps(series, indent=4, ensure_ascii=False))
-
-    # DUMPS IS SUPPOSED TO BE FOR FILES
-    # BUT FILE HAS TO BE OPENED WITH 'W' FIRST
     series_extended = tvdb.get_series_extended(tvdb_id)
-    json.dump(series_extended, outSE.open("w"), indent=4, ensure_ascii=False)
+    series_episodes = tvdb.get_series_episodes(tvdb_id)
+    series_ep_def = tvdb.get_series_episodes(tvdb_id, "default")
+    series_ep_alt = tvdb.get_series_episodes(tvdb_id, "alternate")
+    # series_tranlation = tvdb.get_series_translation(tvdb_id, "english") # NEED TO FIND CORRECT STR
 
-    series_episodes = tvdb.get_series_episodes(tvdb_id, "alternate")
+    # #### OUTPUT INFO
+    # outSeries.write_text(json.dumps(series, indent=4, ensure_ascii=False))
+    output(outSeries, series)
+    output(outSEx, series_extended)
+    output(outSEp, series_episodes)
 
 
 if __name__ == "__main__":
